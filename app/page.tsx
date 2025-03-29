4110a5e93ece4757bf62cc88e0db62b4c19acfe3
Rodrigo Mastropietro <rmpietro@gmail.com>
2025-03-29 18:34:00 -0300

    Configured ESLINT, Prettier and Tailwind in project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl font-black text-violet-700">Welcome to the world of Next JS</h1>
+    <h1 className="text-3xl font-black text-violet-700">
+      Welcome to the world of Next JS
+    </h1>
   );
-}
+};
 
 export default Home;
